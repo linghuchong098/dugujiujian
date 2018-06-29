d030d2a9160c2dddbba4d963310bb748fb007291
linghuchong098 <40692632+linghuchong098@users.noreply.github.com>
2018-06-29 23:57:03 +0800

    独孤九剑更新

--- src/com/atguigu/Dugujiujian.java
+++ src/com/atguigu/Dugujiujian.java
@@ -3,6 +3,8 @@ package com.atguigu;
 public class Dugujiujian {
 	public static void main(String[] args) {
 		System.out.println("hello dugujiujian");
+		System.out.println("hello dugujiujian");
+		System.out.println("hello dugujiujian");
 	}
 
 }
