8bde12f6c8425d5a372cae21ffe72fd30873231d
linghuchong098 <40692632+linghuchong098@users.noreply.github.com>
2018-06-29 23:38:08 +0800

    Update Dugujiujian.java

--- src/com/atguigu/Dugujiujian.java
+++ src/com/atguigu/Dugujiujian.java
@@ -3,6 +3,7 @@ package com.atguigu;
 public class Dugujiujian {
 	public static void main(String[] args) {
 		System.out.println("hello dugujiujian");
+		System.out.println("hello 这是测试GitHub更新")
 	}
 
 }
